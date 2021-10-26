5687e4bed41126101049e9ff7ffb9ca80c1c25ef
kukuduran05 <63382669+kukuduran05@users.noreply.github.com>
2021-10-26 13:52:50 -0500

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Required External Modules
+ */
+const express = require("express");
+
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("Now is running!");
+});
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
